b283bf09cb880893150910c1a0068581beaf4130
Kim Dolleris <kdo@beneath.dk>
2016-06-27 08:50:16 +0200

    newsletterpopup part 2. (still not done)

--- js/scripts.js
+++ js/scripts.js
@@ -2203,8 +2203,9 @@ massachusetts.newsletterpopup = massachusetts.newsletterpopup || function () {
     function init() {
 
         newsletterPopup();
-
+        signupSubmit();
     }
+
 
     // Cookie funtion
     function setNewsletterCookie(key, value) {
@@ -2218,20 +2219,55 @@ massachusetts.newsletterpopup = massachusetts.newsletterpopup || function () {
       return keyValue ? keyValue[2] : null;
     }
 
+    function signupSubmit(){
+        $('#newsletterpopup-submit').submit(function() {
+            var action = $(this).attr('action');
+            loading();
+            $.ajax({
+                url: action,
+                type: 'POST',
+                data: {
+                    email: $('#newsletterpopup-email').val()
+                },
+                success: function(data){
+                    formResult(data);
+                    //$('#mailchimp-signup').hide();
+                    console.log("Succes: ", data);
+                    //you should first check if ga is set
+                    if (typeof ga !== 'undefined') {
+                        ga('send', 'event', 'NewsletterSignup', 'A new signup via websitepopup');
+                     }
+                    //check if _gaq is set too
+                    if (typeof _gaq !== 'undefined') {
+                        _gaq.push(['_trackEvent', 'NewsletterSignup', 'A new signup via websitepopup']);
+                    }
+                },
+                error: function(data) {
+                    formResult(data);
+                    console.log("Error: ", data);
+                }
+            });
+        return false;
+        });
+    }
+
     function newsletterPopup(){
 
-        if (!getNewsletterCookie('massachusettsNewsletterPopup')){
-            $('#newsletterpopup').delay(popupDelay).animate({bottom: "0px"},1000);
-        } else {
+        var numberOfDeactivates = 0;
+
+        if (getNewsletterCookie('massachusettsNewsletterPopup') >= minimumViews){
             $('#newsletterpopup-reactivate').animate({bottom: "0px"},200);
+        } else {
+            $('#newsletterpopup').delay(popupDelay).animate({bottom: "0px"},1000);
         }
 
         $('#newsletterpopup-deactivate').click(function(){
             $('#newsletterpopup').animate({bottom: "-600px"},500,function(){
                 $('#newsletterpopup-reactivate').animate({bottom: "0px"},200);
             });
-            setNewsletterCookie('massachusettsNewsletterPopup','Yes');
-            //console.log('cookie sat til ' + CookieLifeTime + ' dage')
+            numberOfDeactivates = parseInt(getNewsletterCookie('massachusettsNewsletterPopup'));
+            numberOfDeactivates++;
+            setNewsletterCookie('massachusettsNewsletterPopup', numberOfDeactivates);
         });
 
         $('#newsletterpopup-reactivate').click(function(){
@@ -2242,28 +2278,11 @@ massachusetts.newsletterpopup = massachusetts.newsletterpopup || function () {
 
     };
 
-    function signupClick(){
-        //you should first check if ga is set
-        if (typeof ga !== 'undefined') {
-            ga('send', 'event', 'NewsletterSignup', 'A new signup via websitepopup');
-         }
-        //check if _gaq is set too
-        if (typeof _gaq !== 'undefined') {
-            _gaq.push(['_trackEvent', 'NewsletterSignup', 'A new signup via websitepopup']);
-        }
-    }
-
     return {
         init:init
     };
 
 }();
-
-
-
-
-
-
 
 
 // Doc ready
